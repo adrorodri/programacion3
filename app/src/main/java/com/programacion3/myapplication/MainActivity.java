bdce9e3d442a730622be128a54927d1f346d1161
adrorodri <adrorodri@gmail.com>
2018-04-06 11:22:13 -0400

    Two Activities and LifeCycles

--- app/src/main/java/com/programacion3/myapplication/MainActivity.java
+++ app/src/main/java/com/programacion3/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.programacion3.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LIFE CYCLE EVENTS", "ON CREATE (1)");
 
         textViewTest = findViewById(R.id.testJavaText);
 
@@ -37,6 +40,47 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickAqui(View view) {
         Toast.makeText(this, "Pollito Click Listener", Toast.LENGTH_SHORT).show();
+    }
+
+    public void openNewActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFE CYCLE EVENTS", "ON START (1)");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFE CYCLE EVENTS", "ON RESUME (1)");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFE CYCLE EVENTS", "ON PAUSE (1)");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFE CYCLE EVENTS", "ON STOP (1)");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFE CYCLE EVENTS", "ON DESTROY (1)");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LIFE CYCLE EVENTS", "ON RESTART (1)");
     }
 }
 
---
+++ app/src/main/java/com/programacion3/myapplication/SecondActivity.java
@@ -0,0 +1,69 @@
+package com.programacion3.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d("LIFE CYCLE EVENTS", "ON CREATE (2)");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LIFE CYCLE EVENTS", "ON START (2)");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LIFE CYCLE EVENTS", "ON RESUME (2)");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LIFE CYCLE EVENTS", "ON PAUSE (2)");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LIFE CYCLE EVENTS", "ON STOP (2)");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LIFE CYCLE EVENTS", "ON DESTROY (2)");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LIFE CYCLE EVENTS", "ON RESTART (2)");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
